44a8217adaae30c8d905fd9a277a87af0b0a70ec
Sergey <Bubovich>
2013-09-02 00:39:04 +0300

    package dcterms:modified meta element must occur exactly once

--- lib/eeepub/opf.rb
+++ lib/eeepub/opf.rb
@@ -82,9 +82,7 @@ module EeePub
             end
           end
         end
-        builder.meta(:property => 'dcterms:modified') do
-          Time.now.strftime('%FT%TZ')
-        end
+        builder.meta({:property => 'dcterms:modified'}, Time.now.strftime('%FT%TZ'))
         builder.meta(:name => 'cover', :content => self.cover) if self.cover
       end
     end
